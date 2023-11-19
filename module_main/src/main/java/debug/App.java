e3a2e32ef7b7be07b427c210e280aead81137170
xoliu1 <xoliu6@gmail.com>
2023-11-19 23:22:02 +0800

    完成主界面的基本框架

--- module_main/src/main/java/debug/App.java
+++ module_main/src/main/java/debug/App.java
@@ -1,7 +1,9 @@
 package debug;
 
-import com.xoliu.common.Base.BaseApplication;
+import android.app.Application;
 
-public class App extends BaseApplication {
+
+
+public class App extends Application {
     //实现自定义模块的部分变量初始化
 }

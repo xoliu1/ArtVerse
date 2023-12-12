af30abf13078722d6fe82a1c25ca149ade36bf4d
xoliu1 <xoliu6@gmail.com>
2023-12-12 19:31:58 +0800

    完成收藏界面

--- module_profile/src/main/java/com/xoliu/module_profile/fragment_profile_main.java
+++ module_profile/src/main/java/com/xoliu/module_profile/fragment_profile_main.java
@@ -1,19 +1,36 @@
 package com.xoliu.module_profile;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.room.Room;
 
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.alibaba.android.arouter.facade.annotation.Route;
+import com.bumptech.glide.Glide;
 import com.xoliu.module_profile.databinding.FragmentProfileMainBinding;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import db.AppDatabase;
+import db.bean.PoemCard;
 
 @Route(path = "/profile/main")
 public class fragment_profile_main extends Fragment {
-
+    private FragmentProfileMainBinding binding;
 
     public fragment_profile_main() {
     }
@@ -22,17 +39,53 @@ public class fragment_profile_main extends Fragment {
         fragment_profile_main fragment = new fragment_profile_main();
         return fragment;
     }
-
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
+    public void onCreate(Bundle savedInstanceState) {super.onCreate(savedInstanceState);}
+
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        FragmentProfileMainBinding binding =  FragmentProfileMainBinding.inflate(inflater);
+        binding =  FragmentProfileMainBinding.inflate(inflater);
         return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        initData();
+        initView();
+    }
+
+
+
+
+    private void initView() {
+        binding.profileUserIcon.setOnClickListener(v -> {
+            startActivity(new Intent(getContext(), ImagePickerActivity.class));
+        });
+
+        List<PoemCard> poemCardList = new ArrayList<>();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                AppDatabase db = Room.databaseBuilder(getContext(), AppDatabase.class, "PoemCards").build();
+                List<PoemCard> list = db.poemCardDao().getAllPoemCards();
+                for (PoemCard card : list) {
+                    poemCardList.add(card);
+                }
+
+            }
+        }).start();
+        binding.profileRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        binding.profileRecyclerView.setAdapter(new PoemCardAdapter(poemCardList));
+
+    }
+
+
+
+    private void initData() {
     }
 }
